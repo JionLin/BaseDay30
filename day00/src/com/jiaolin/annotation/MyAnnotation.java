776406fe3252b61d75c9103da97e5b175120dde6
JionLin <250912986@qq.com>
2022-07-19 18:41:50 +0800

    补充的代码知识点。

---
+++ day00/src/com/jiaolin/annotation/MyAnnotation.java
@@ -0,0 +1,10 @@
+package com.jiaolin.annotation;
+
+/**
+ * @author johnny
+ * @Classname MyAnnotation
+ * @Description 后续看宋洪康的资料,补充的代码知识点。
+ * @Date 2022/7/19 18:40
+ */
+public class MyAnnotation {
+}
