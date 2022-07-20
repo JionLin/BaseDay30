80abc24958a623535b64c5f5062558a1b8b0b9cf
JionLin <250912986@qq.com>
2022-07-20 18:54:47 +0800

    泛型在继承中的体现。 G<a>不是G<B>的子类。

--- day00/src/com/jiaolin/generic/PersonTest.java
+++ day00/src/com/jiaolin/generic/PersonTest.java
@@ -1,18 +1,32 @@
 package com.jiaolin.generic;
 
+import java.util.List;
+
 /**
  * @author johnny
  * @Classname PersonTest 如果为 <T extends  Person3> 的形式,那么这个T 必须 <= person3 才行
- * @Description
+ * @Description 泛型在类型中的体现 如果B是A的一个子类型,而G是具有泛型的类或者接口,那么G<B>不是G<A>的子类型,从 testGenericAndSubClass可以看出
  * @Date 2022/7/20 18:43
  */
 public class PersonTest {
-    public static <T extends  Person3> void test(T t){
+    public static <T extends Person3> void test(T t) {
         System.out.println(t);
     }
 
 
+    public void testGenericAndSubClass() {
+        List<Person3> list = null;
+        List<Man> list1 = null;
+        // 那么G<B>不是G<A>的子类型
+        // list=list1;
 
+        // Person3 是 Man的父类
+        Person3[] person3s = null;
+        Man[] mans = null;
+        person3s = mans;
+
+
+    }
 
 
     public static void main(String[] args) {
@@ -23,9 +37,12 @@ public class PersonTest {
     }
 }
 
-class Creature{}
+class Creature {
+}
 
-class Person3 extends  Creature{}
+class Person3 extends Creature {
+}
 
-class Man extends  Person3{}
+class Man extends Person3 {
+}
 
