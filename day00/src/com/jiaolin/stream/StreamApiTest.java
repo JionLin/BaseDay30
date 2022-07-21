69d4d1e1d1532f979b323a2beb63a5be735deb55
JionLin <250912986@qq.com>
2022-07-21 08:43:04 +0800

    stream api 学习。

---
+++ day00/src/com/jiaolin/stream/StreamApiTest.java
@@ -0,0 +1,17 @@
+package com.jiaolin.stream;
+
+/**
+ * @author johnny
+ * @Classname StreamApiTest
+ * @Description stream API 表达式 分为3个操作
+ * 创建 4种形式
+ *      list 数组 stream 以及stream 类 无限流
+ * 中间操作 3种形式
+ *      筛选及切片
+ *      映射
+ *
+ * 终止操作
+ * @Date 2022/7/21 08:40
+ */
+public class StreamApiTest {
+}
