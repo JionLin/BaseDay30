5f74c9c3e451513baa4a84aa3b39147040f58466
JionLin <250912986@qq.com>
2022-07-22 10:36:06 +0800

    lambda 表达式接口需要继承父类,本类并没有对应的方法,那么可以看对应的接口中的入参和父接口的参数进行比较,就知道入参和返回的参数了。

--- day00/src/com/jiaolin/lambda/LambdaApiTest1.java
+++ day00/src/com/jiaolin/lambda/LambdaApiTest1.java
@@ -8,31 +8,27 @@ package com.jiaolin.lambda;
  */
 public class LambdaApiTest1 {
     /**
-     函数式接口
-     参数类型
-     返回类型
-     用途
+
+     可以理解
      BiFunction<T, U, R>
      T, U
      R
      对类型为 T, U 参数应用操作，返回 R 类型的结 果。包含方法为: R apply(T t, U u);
+     R apply(T t, U u);
 
-     函数式接口
-     参数类型
-     返回类型
-     用途
-     UnaryOperator<T> (Function子接口)
+
+
+     可以理解
+     UnaryOperator<T> (Function子接口 R apply(T t)的参数 )  那么子类为 入参为t 返回参数为t
      T
      T
      对类型为T的对象进行一元运算，并返回T类型的 结果。包含方法为:T apply(T t);
 
 
-     函数式接口
-     参数类型
-     返回类型
-     用途
-     BinaryOperator<T> (BiFunction 子接口)
-     T, T
+
+     可以理解
+     BinaryOperator<T>(BiFunction<T,T,T> )   (BiFunction 子接口    R apply(T t, U u); )
+     T, T 入参
      T
      对类型为T的对象进行二元运算，并返回T类型的 结果。包含方法为: T apply(T t1, T t2);
      BiConsumer<T, U>
@@ -41,28 +37,21 @@ public class LambdaApiTest1 {
      对类型为T, U 参数应用操作。 包含方法为: void accept(T t, U u)
 
 
-     函数式接口
-     参数类型
-     返回类型
-     用途
+
+     可以理解
      BiPredicate<T,U>
      T,U
      boolean
      包含方法为: boolean test(T t,U u)
 
-     函数式接口
-     参数类型
-     返回类型
-     用途
+     可以理解
      ToIntFunction<T> ToLongFunction<T> ToDoubleFunction<T>
      T
      int long double
      分别计算int、long、double值的函数
 
-     函数式接口
-     参数类型
-     返回类型
-     用途
+
+     可以理解
      IntFunction<R> LongFunction<R> DoubleFunction<R>
      int long double
      R
