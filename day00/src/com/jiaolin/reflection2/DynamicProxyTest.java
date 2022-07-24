59624f9d87aca831436fde0b306b3f09a8748a59
JionLin <250912986@qq.com>
2022-07-24 11:13:56 +0800

    动态代理 会产生的原因, 1。怎么进行和动态代理类的绑定 Proxy.newInstance()方法进行 2。怎么调用代理类的方法。 调用invoke方法进行。

--- day00/src/com/jiaolin/reflection2/DynamicProxyTest.java
+++ day00/src/com/jiaolin/reflection2/DynamicProxyTest.java
@@ -1,5 +1,10 @@
 package com.jiaolin.reflection2;
 
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * @author johnny
  * @Classname DynmicProxyTest
@@ -7,4 +12,65 @@ package com.jiaolin.reflection2;
  * @Date 2022/7/24 10:05
  */
 public class DynamicProxyTest {
+    public static void main(String[] args) {
+        Teacher teacher=new Teacher();
+        Teach teach = (Teach) ProxyFactory2.getInstance(teacher);
+        teach.teach();
+        String eat = teach.eat("水果");
+        System.out.println(eat);
+
+    }
+}
+
+interface Teach {
+
+    void teach();
+
+    String eat(String name);
+}
+
+
+// 被代理类
+class Teacher implements Teach {
+
+    @Override
+    public void teach() {
+        System.out.println("老师教书");
+    }
+
+    @Override
+    public String eat(String foodName) {
+        System.out.println("老师吃 " + foodName);
+        return foodName;
+    }
+}
+
+/**
+ * 代理类 2个原因
+ * 1。如何产生一个代理对象,并进行绑定
+ * 2。如何调用代理类中的方法
+ */
+class ProxyFactory2 {
+
+    public static Object getInstance(Object obj) {
+        MyInvocationHandler handler = new MyInvocationHandler();
+        handler.band(obj);
+        return Proxy.newProxyInstance(obj.getClass().getClassLoader(),
+                obj.getClass().getInterfaces(), handler);
+    }
+
+}
+
+class MyInvocationHandler implements InvocationHandler {
+
+    private Object obj;
+
+    public void band(Object obj) {
+        this.obj = obj;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return method.invoke(obj, args);
+    }
 }
